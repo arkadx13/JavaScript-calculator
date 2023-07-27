f99f30a24b5b6970307d69007f818d3299d8922e
Mariel Juliene Macabudbud <arkadx213@gmail.com>
2023-07-27 18:48:08 +0800

    updating ID's and classNames

--- App.js
+++ App.js
@@ -11,36 +11,72 @@ function App() {
 function Keypad() {
   return (
     <div className="keypad">
-      <button className="key ac">AC</button>
+      <button id="clear" className="key ac">
+        AC
+      </button>
       <button className="key delete">←</button>
-      <button className="key operator">x</button>
+      <button id="multiply" className="key operator">
+        x
+      </button>
 
-      <button className="key">7</button>
-      <button className="key">8</button>
-      <button className="key">9</button>
-      <button className="key operator">/</button>
+      <button id="seven" className="key">
+        7
+      </button>
+      <button id="eight" className="key">
+        8
+      </button>
+      <button id="nine" className="key">
+        9
+      </button>
+      <button id="divide" className="key operator">
+        /
+      </button>
 
-      <button className="key">4</button>
-      <button className="key">5</button>
-      <button className="key">6</button>
-      <button className="key operator">-</button>
+      <button id="four" className="key">
+        4
+      </button>
+      <button id="five" className="key">
+        5
+      </button>
+      <button id="six" className="key">
+        6
+      </button>
+      <button id="subtract" className="key operator">
+        -
+      </button>
 
-      <button className="key">1</button>
-      <button className="key">2</button>
-      <button className="key">3</button>
-      <button className="key operator">+</button>
+      <button id="one" className="key">
+        1
+      </button>
+      <button id="two" className="key">
+        2
+      </button>
+      <button id="three" className="key">
+        3
+      </button>
+      <button id="add" className="key operator">
+        +
+      </button>
 
-      <button className="key zero">0</button>
-      <button className="key">.</button>
-      <button className="key operator">=</button>
+      <button id="zero" className="key zero">
+        0
+      </button>
+      <button id="decimal" className="key">
+        .
+      </button>
+      <button id="equals" className="key operator">
+        =
+      </button>
     </div>
   );
 }
 function Display() {
   return (
-    <div className="display">
+    <div className="display-container">
       <div className="expressions">1x4</div>
-      <div className="answer">4444444445678</div>
+      <div id="display" className="answer">
+        0
+      </div>
     </div>
   );
 }
